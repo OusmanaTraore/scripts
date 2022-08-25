bed0f7d60d47e0a58c39a8b1994166eb4752b63f
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-08-25 13:10:08 +0200

    Update 01-ExportToCSV.sh

--- CONVERT/01-ExportToCSV.sh
+++ CONVERT/01-ExportToCSV.sh
@@ -9,7 +9,6 @@ echo " | Enter the name of table to export  >"
 read -p " |> " table_name
 export $table_name
 
-
 #cat << EOF >
 #SELECT * FROM $table_name
 #INTO OUTFILE '/var/lib/mysql-files/$file_name.csv'
@@ -20,7 +19,7 @@ export $table_name
 sleep 2
 echo " | Exporting table: $table_name to csv file  >"
 
-mysql  -u $user_db -p $db_name -e 'select * from NAGIOSTATS' |
+mysql  -u $user_db -p $db_name -e 'select * from' $table_name |
 sed 's/\t/,/g' > $file_name.csv
 
 cat $file_name.csv | head -10
