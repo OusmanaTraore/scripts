c238aa8049a4c670d53a73695b97dea5e2041f37
Ousmana Traore <traoreosman@yahoo.fr>
2022-10-08 21:25:47 +0200

    adding PROGRAM to repository

---
+++ PROGRAM/GRAFANA EXPORT PDF/fonctions.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+source variables.sh
+
+###  pause 
+pause(){
+    echo "Appuyer sur une touche pour continuer!"
+    read x
+}
+####################### DEBUT INSTALLATION  ############################
+etape=1
+# var="ls $dir"
+intro(){
+    echo "======================================================="
+    echo "          Lancement du program $dir"
+    echo "    |====>                                   <=====|   "
+    echo ""
+    sleep 2 
+    for filename in 0*
+    do 
+         echo "=================================================="
+         echo -n "STEP n°$etape"
+         echo ""
+         echo -n " |===>  "; echo $filename | cut -d "-" -f 2 | cut -d "." -f 1  | awk '{ print toupper($0) }'         
+         echo "=================================================="
+         ((etape++))
+    done
+
+}
+procedure(){
+    echo -n "
+    Choose the step to install,
+    When you choose one step, 
+    please make sure all previous step have been already done!
+    
+    "
+    #echo -n "You can check the step by choosing CHECK STEP!
+    #"
+    echo ""
+    select filename in 0* 
+    do
+        echo "$filename"
+        bash $filename
+    done
+}
+
+addingGit(){
+    if [[ ! -f .git ]]
+    then
+        git init
+    fi
+        echo -n "Choose to 'pull git' or 'push to git' ? 
+        pg=pullGit, 
+        pgt=pushToGit"
+
+        echo ""
+        read -p "|==> " x
+        if [[ $x == "pg"  ]] 
+        then 
+            git pull git@github.com:OusmanaTraore/scripts.git
+        elif [[ $x == "ptg"  ]]  
+        then
+            echo  "Push to git ? , yes or NO! " 
+            read -p "|==> " pushGit
+            if [[ $pushGit == "yes"  ]] || [[ $pushGit == "y"  ]] || [[ $pushGit == "o"  ]]
+            then
+            echo "Add files to push : file1 file2 ..." 
+            read -p "|==> " file
+            git config --global user.name "Ousmana Traore"
+            git config --global user.email "traoreosman@yahoo.fr"
+            for i in $file
+            do 
+                git add $i
+            done
+            
+            git remote add origin git@github.com:OusmanaTraore/scripts.git
+            git commit -m "adding $file to repository"
+            ###
+            sleep 2
+            echo " |==> GENERATING SSHKEY"
+            ssh-keygen -t ed25519 -N '' -f ~/.ssh/gitpush
+            eval $(ssh-agent -s)
+            ssh-add ~/.ssh/gitpush
+            cat ~/.ssh/gitpush.pub
+            echo " add the key to your repository , then press key to continue" 
+            read -p " |==> " k
+
+            sleep 2 
+            git push -u origin master
+            else
+            echo "You choose not to push in git repository"
+            fi
+        else 
+            echo "Sorry you pressed wrong key !!!"
+        fi
+
+}
+####################### FIN INSTALLATION  ############################
---
+++ PROGRAM/GRAFANA EXPORT PDF/procedure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source fonctions.sh variables.sh
+echo "loading ..."
+sleep 2
+bash intro.sh
+intro
+sleep 2
+procedure
